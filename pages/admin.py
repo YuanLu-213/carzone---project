99c6bd64eff39bf5afc21938a9d8d914b784bdbc
Yuan Lu <lu213@uw.edu>
2022-01-14 00:39:08 -0800

    admin page created and team model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+
+class Teamadmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;"/>'.format(object.photos.url))
+
+    thumbnail.short_description = 'photo'
+    list_display = ('first_name', 'thumbnail', 'designation', 'created_data')
+    list_display_links = ('first_name', 'thumbnail')
+    search_fields = ('first_name', 'last_name', 'designation')
+    list_filter = ('designation', )
+
+admin.site.register(Team, Teamadmin)
